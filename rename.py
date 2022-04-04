5d633beaff9250d07cbc827576bdac79d0939267
Dev-cSharpe <41975177+Dev-cSharpe@users.noreply.github.com>
2022-04-05 01:06:14 +0530

    replace product images

---
+++ rename.py
@@ -0,0 +1,6 @@
+import os
+_src = r"/Users/mukeshkudi/Desktop/data/First_project_FromScratch/Product_img/"
+_ext = ".jpg"
+for i,filename in enumerate(os.listdir(_src)):
+    if filename.endswith(_ext):
+        os.rename(_src+filename,_src+str(i)+_ext)
